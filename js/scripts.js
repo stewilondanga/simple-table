c1756d6dd8bddba16ec0401f4da9d0a5cb9f00d6
Stewart ilondanga <stilondanga@gmail.com>
2020-09-16 13:20:16 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
 
-document.getElementById('tbody').addEventListener("mouseover",(e)=>{
+document.getElementById('tbody').addEventListener("mouseover", (e) => {
 
-    let cellIndex= e.target.cellIndex;
-    let teerki = document.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
-    let tedes = document.getElementsByTagName('td')
+  let cellIndex = e.target.cellIndex;
+  let teerki = document.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+  let tedes = document.getElementsByTagName('td')
 
-    for(let i = 0; i< document.getElementsByTagName('td').length; i++){
-      document.getElementsByTagName('td')[i].style.backgroundColor="transparent";
+  for (let i = 0; i < document.getElementsByTagName('td').length; i++) {
+    document.getElementsByTagName('td')[i].style.backgroundColor = "transparent";
 
-    }
-    for(let i=0; i<teerki.length; i++){
-      teerki[i].getElementsByTagName('td')[cellIndex].style.backgroundColor="rgba(0,0,255,0.2)";
-    }
-    document.getElementsByTagName('thead')
-  })
+  }
+  for (let i = 0; i < teerki.length; i++) {
+    teerki[i].getElementsByTagName('td')[cellIndex].style.backgroundColor = "rgba(0,0,255,0.2)";
+  }
+  document.getElementsByTagName('thead')
+})
