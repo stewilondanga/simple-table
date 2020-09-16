21ee0a03781c3f91add95384db018081a9efc73c
Stewart ilondanga <stilondanga@gmail.com>
2020-09-16 13:19:43 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,19 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+document.getElementById('tbody').addEventListener("mouseover",(e)=>{
+
+    let cellIndex= e.target.cellIndex;
+    let teerki = document.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+    let tedes = document.getElementsByTagName('td')
+
+    for(let i = 0; i< document.getElementsByTagName('td').length; i++){
+      document.getElementsByTagName('td')[i].style.backgroundColor="transparent";
+
+    }
+    for(let i=0; i<teerki.length; i++){
+      teerki[i].getElementsByTagName('td')[cellIndex].style.backgroundColor="rgba(0,0,255,0.2)";
+    }
+    document.getElementsByTagName('thead')
+  })
